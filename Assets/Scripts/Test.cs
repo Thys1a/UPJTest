cd5d1fe6fcc86af616f604dc4bc95c0d08035163
wusanfengq <105572291+wusanfengq@users.noreply.github.com>
2022-10-05 18:32:37 +0800

    clueEntity

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        //use for testing 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //use for testing 
-    }
+{    
+  
 }
